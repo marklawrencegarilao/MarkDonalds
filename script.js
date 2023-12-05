cc1d0341a38e0d7a20f1a4a9e51bb80ffd5036b5
marklawrencegarilao <argonrhg@Gmail.com>
2023-12-06 07:27:28 +0800

    added slick js and making menu

---
+++ script.js
@@ -0,0 +1,23 @@
+
+$('.slides').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoplaySpeed: 600,
+    speed: 2000
+});
+
+$('.imagede').slick({
+    centerMode: true,
+    centerPadding: '100vw',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 800,
+    arrows: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+})
